724bdc081eabce890655d02823f64df6803bff39
phmx17 <slimyelow@gmail.com>
2020-12-05 19:04:27 +0100

    made first change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: red;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
