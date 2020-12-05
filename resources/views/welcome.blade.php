4fc38c4d8c71c7b53cc359b126b506009f8d6b57
phmx17 <slimyelow@gmail.com>
2020-12-05 19:14:15 +0100

    changed color to red and back

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: red;
+                background-color:#ccc;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
